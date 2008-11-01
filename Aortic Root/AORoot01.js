02edb1c97b1ae1927c2e6281e93c465df67ac2b3
dan.dyar <dan.dyar@5c7b53a3-c253-0410-9ef7-7744691651de>
2008-11-01 16:16:43 +0000

    added calculation of centiles

--- Aortic Root/AORoot01.js
+++ Aortic Root/AORoot01.js
@@ -38,9 +38,9 @@ function calcAVA() {
     var AVA = parseFloat(document.getElementById("txtAVA").value);
     if (!isNaN(AVA)) {
         var zScore = getZScore(AVA, MYECHO.AVAmean, MYECHO.AVAsd);
-        document.getElementById("AVAZ").innerHTML = zScore
+        document.getElementById("AVAZ").innerHTML = zScore;
         document.getElementById("AVAZ").className = [ZscoreFlag(zScore)];
-
+        document.getElementById("AVAPercentile").innerHTML = calcPercentile(zScore);
     }
     
 }
@@ -50,6 +50,8 @@ function calcRoot() {
         var zScore = getZScore(ROOT, MYECHO.ROOTmean, MYECHO.ROOTsd);
         document.getElementById("RootZ").innerHTML = zScore;
         document.getElementById("RootZ").className = [ZscoreFlag(zScore)];
+        document.getElementById("RootPercentile").innerHTML = calcPercentile(zScore);
+
     }
 }
 
@@ -84,6 +86,56 @@ function ZscoreFlag(zScore) {
     }
     else return "normal";
 }
+//Percentiles function for z-scores (adapted by Dan Dyar)
+//The following JavaScript functions for calculating normal and
+//chi-square probabilities and critical values were adapted by
+//John Walker from C implementations
+//written by Gary Perlman of Wang Institute, Tyngsboro, MA
+//01879.  Both the original C code and this JavaScript edition
+//are in the public domain.
+
+function calcPercentile(z) {
+    if (z == 0) {var qz = 0.5}
+    else if (z > -6 && z < 6) {
+        var qz = poz(z);}
+    else {//the z score is beyond 6
+        return "*";}
+    return (qz * 100).toFixed(2);
+
+}//end Percentiles fx
+
+//poz ---  probability of normal z value
+function poz(z) {
+    var y, x, w;
+    var Z_MAX = 6;
+    if (z == 0.0) {
+        x = 0.0;
+    } else {
+        y = 0.5 * Math.abs(z);
+        if (y > (Z_MAX * 0.5)) {
+            x = 1.0;
+        } else if (y < 1.0) {
+            w = y * y;
+            x = ((((((((0.000124818987 * w
+                     - 0.001075204047) * w + 0.005198775019) * w
+                     - 0.019198292004) * w + 0.059054035642) * w
+                     - 0.151968751364) * w + 0.319152932694) * w
+                     - 0.531923007300) * w + 0.797884560593) * y * 2.0;
+        } else {
+            y -= 2.0;
+            x = (((((((((((((-0.000045255659 * y
+                           + 0.000152529290) * y - 0.000019538132) * y
+                           - 0.000676904986) * y + 0.001390604284) * y
+                           - 0.000794620820) * y - 0.002034254874) * y
+                           + 0.006549791214) * y - 0.010557625006) * y
+                           + 0.011630447319) * y - 0.009279453341) * y
+                           + 0.005353579108) * y - 0.002141268741) * y
+                           + 0.000535310849) * y + 0.999936657524;
+        }
+    }
+    return z > 0.0 ? ((x + 1.0) * 0.5) : ((1.0 - x) * 0.5);
+}// end poz
+
 //Copyright (c) 2008: Dan Dyar
 
 //Permission is hereby granted, free of charge, to any person obtaining a copy
