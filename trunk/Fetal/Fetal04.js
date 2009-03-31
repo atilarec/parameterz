755d6696ce625d09b0cfda447edd700bf8b9f4a2
dan.dyar <dan.dyar@5c7b53a3-c253-0410-9ef7-7744691651de>
2009-03-31 13:21:14 +0000

    added lmp() and edd() methods to fetal object

--- trunk/Fetal/Fetal04.js
+++ trunk/Fetal/Fetal04.js
@@ -1,5 +1,71 @@
 ﻿var fetal = {
     "ega": 18,
+    "lmp": function() {
+        //setup variables for dates: today, LMP, and EDD
+        //initially, they will ALL be set to TODAY
+        var dtToday = new Date();
+        var dtLMP = new Date();
+        var dtEDD = new Date();
+
+        //and set today to noon
+        dtToday.setHours(12, 0, 0, 0);
+
+        //parse the input into an integer value
+        var EGAw = fetal.ega;
+        //find the decimal remainder if any
+        var EGAd = parseFloat(fetal.ega - EGAw); //this is the 'decimal fraction' i.e., days
+
+        //convert the EGA weeks into milliseconds
+        EGAw = EGAw * 7; //weeks into days
+        EGAw = EGAw * 24; //hours per day
+        EGAw = EGAw * 60; //minutes per hr
+        EGAw = EGAw * 60; //secs per minute
+        EGAw = EGAw * 1000; //msec per sec
+
+        EGAd = 7 * EGAd; // get the number of days
+        ////convert the days to msec
+        EGAd = EGAd * 24 * 60 * 60 * 1000;
+
+        //add the two togeter
+        var EGA = EGAw + EGAd;
+
+        //subtract the EGA from today for LMP
+        dtLMP.setTime(dtToday.getTime() - EGA);
+        return dtLMP.toDateString();
+    },
+    "edd": function() {
+        var gestation = new Number
+        var dtToday = new Date();
+        //and set today to noon
+        dtToday.setHours(12, 0, 0, 0);
+
+        //set up the 40 week gestation in ms to get the remaining time of the gestation
+        gestation = 40 * 7 * 24 * 60 * 60 * 1000;
+        //parse the input into an integer value
+        var EGAw = fetal.ega;
+        //find the decimal remainder if any
+        var EGAd = parseFloat(fetal.ega - EGAw); //this is the 'decimal fraction' i.e., days
+
+        //convert the EGA weeks into milliseconds
+        EGAw = EGAw * 7; //weeks into days
+        EGAw = EGAw * 24; //hours per day
+        EGAw = EGAw * 60; //minutes per hr
+        EGAw = EGAw * 60; //secs per minute
+        EGAw = EGAw * 1000; //msec per sec
+
+        EGAd = 7 * EGAd; // get the number of days
+        ////convert the days to msec
+        EGAd = EGAd * 24 * 60 * 60 * 1000;
+
+        //add the two togeter
+        var EGA = EGAw + EGAd;
+        //the EDD is today plus the difference between a 40 week gestation and the time already passed (EGA)
+        var remainingTime = new Number
+        remainingTime = gestation - EGA;
+        var dtEDD = new Date;
+        dtEDD.setTime(dtToday.getTime() + remainingTime);
+        return dtEDD.toDateString();
+    },
     "mitral": {
         "intercept": -4.084,
         "multiplier": 1.173,
@@ -188,12 +254,30 @@
         }
     },
     "getZScore": function(site, score) {
-            var predicted = site.multiplier * Math.log(fetal.ega) + site.intercept;
-            return (Math.log(score) - predicted) / site.rmse;
+        var predicted = site.multiplier * Math.log(fetal.ega) + site.intercept;
+        return (Math.log(score) - predicted) / site.rmse;
     },
     "getMeasurement": function(site, zscore) {
-            var predicted = site.multiplier * Math.log(fetal.ega) + site.intercept;
-            return Math.exp(predicted + (zscore * site.rmse));
+        var predicted = site.multiplier * Math.log(fetal.ega) + site.intercept;
+        return Math.exp(predicted + (zscore * site.rmse));
     }
 };
+//Copyright (c) 2009: Dan Dyar
 
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
