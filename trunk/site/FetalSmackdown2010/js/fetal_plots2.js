75c1ed6389ad5a952b3e3bd9c751b35fe30544c0
dan.dyar <dan.dyar@5c7b53a3-c253-0410-9ef7-7744691651de>
2009-12-30 06:51:00 +0000

    new fetal smackdown calculator/plottting tool

---
+++ trunk/site/FetalSmackdown2010/js/fetal_plots2.js
@@ -0,0 +1,230 @@
+﻿var Boston_aov_data = [
+    {
+        label: "Boston AoV Upper",
+        color: 1,
+        lines: { show: true, fill: true },
+        data: [[16, 0.261],
+            [17, 0.288],
+            [18, 0.316],
+            [19, 0.343],
+            [20, 0.371],
+            [21, 0.398],
+            [22, 0.426],
+            [23, 0.453],
+            [24, 0.481],
+            [25, 0.509],
+            [26, 0.536],
+            [27, 0.564],
+            [28, 0.591],
+            [29, 0.619],
+            [30, 0.646],
+            [31, 0.674],
+            [32, 0.701],
+            [33, 0.729],
+            [34, 0.757],
+            [35, 0.784],
+            [36, 0.812],
+            [37, 0.839],
+            [38, 0.867],
+            [39, 0.894],
+            [40, 0.922]]
+    },
+            {
+                label: "Boston AoV Lower",
+                color: 1,
+                lines: { show: true, fill: true, fillColor: "rgba(255, 255, 255, 0.8)" },
+                data: [[16, 0.169],
+                [17, 0.190],
+                [18, 0.211],
+                [19, 0.231],
+                [20, 0.252],
+                [21, 0.273],
+                [22, 0.294],
+                [23, 0.314],
+                [24, 0.335],
+                [25, 0.356],
+                [26, 0.377],
+                [27, 0.397],
+                [28, 0.418],
+                [29, 0.439],
+                [30, 0.460],
+                [31, 0.480],
+                [32, 0.501],
+                [33, 0.522],
+                [34, 0.543],
+                [35, 0.563],
+                [36, 0.584],
+                [37, 0.605],
+                [38, 0.626],
+                [39, 0.646],
+                [40, 0.667]]
+}];
+var Royal_aov_data = [{
+    label: "Royal Brompton AoV Upper",
+    color: 1,
+    lines: { show: true, fill: true },
+    data: [[16, 0.271],
+                [17, 0.293],
+                [18, 0.314],
+                [19, 0.337],
+                [20, 0.359],
+                [21, 0.382],
+                [22, 0.405],
+                [23, 0.429],
+                [24, 0.452],
+                [25, 0.476],
+                [26, 0.500],
+                [27, 0.525],
+                [28, 0.549],
+                [29, 0.574],
+                [30, 0.599],
+                [31, 0.625],
+                [32, 0.650],
+                [33, 0.676],
+                [34, 0.702],
+                [35, 0.728],
+                [36, 0.755],
+                [37, 0.781],
+                [38, 0.808],
+                [39, 0.835],
+                [40, 0.862]]
+},
+            {
+                label: "Royal Brompton AoV Lower",
+                color: 1,
+                lines: { show: true, fill: true, fillColor: "rgba(255, 255, 255, 0.8)" },
+                data: [[16, 0.178],
+                [17, 0.192],
+                [18, 0.206],
+                [19, 0.221],
+                [20, 0.235],
+                [21, 0.250],
+                [22, 0.265],
+                [23, 0.281],
+                [24, 0.296],
+                [25, 0.312],
+                [26, 0.328],
+                [27, 0.344],
+                [28, 0.360],
+                [29, 0.376],
+                [30, 0.393],
+                [31, 0.409],
+                [32, 0.426],
+                [33, 0.443],
+                [34, 0.460],
+                [35, 0.477],
+                [36, 0.494],
+                [37, 0.512],
+                [38, 0.529],
+                [39, 0.547],
+                [40, 0.565]]
+}];
+var Michigan_aov_data = [
+        { label: "Michigan AoV Upper",
+            color: 1,
+            lines: { show: true, fill: true },
+            data: [[16, 0.256],
+            [17, 0.287],
+            [18, 0.319],
+            [19, 0.350],
+            [20, 0.382],
+            [21, 0.414],
+            [22, 0.445],
+            [23, 0.477],
+            [24, 0.508],
+            [25, 0.540],
+            [26, 0.571],
+            [27, 0.603],
+            [28, 0.634],
+            [29, 0.666],
+            [30, 0.697],
+            [31, 0.729],
+            [32, 0.760],
+            [33, 0.792],
+            [34, 0.823],
+            [35, 0.855],
+            [36, 0.886],
+            [37, 0.918],
+            [38, 0.949],
+            [39, 0.981],
+            [40, 1.012]]
+        },
+        {
+            label: "Michigan AoV Lower",
+            color: 1,
+            lines: { show: true, fill: true, fillColor: "rgba(255, 255, 255, 0.8)" },
+            data: [[16, 0.148],
+            [17, 0.174],
+            [18, 0.200],
+            [19, 0.226],
+            [20, 0.252],
+            [21, 0.278],
+            [22, 0.304],
+            [23, 0.330],
+            [24, 0.356],
+            [25, 0.382],
+            [26, 0.408],
+            [27, 0.435],
+            [28, 0.461],
+            [29, 0.487],
+            [30, 0.513],
+            [31, 0.539],
+            [32, 0.565],
+            [33, 0.591],
+            [34, 0.617],
+            [35, 0.643],
+            [36, 0.669],
+            [37, 0.695],
+            [38, 0.721],
+            [39, 0.747],
+            [40, 0.774]]
+            }];
+
+function plotIt(array, ref) {
+    var myData = {
+        label: "your data",
+        color: "#000000",
+        data: [array],
+        points: { show: true },
+        lines: { show: false }
+    };
+    switch (ref) {
+        case "Royal": Choice_data = Royal_aov_data;
+            break;
+        case "Boston": Choice_data = Boston_aov_data;
+            break;
+        case "Michigan": Choice_data = Michigan_aov_data;
+            break;
+    }
+    var plotData = jQuery.extend(true, [], Choice_data); //makes a NEW array from the reference data
+
+    plotData.push(myData); //puts 'myData' in with the reference data
+
+    $.plot($("#aov-placeholder"), plotData, {
+        yaxis: { min: 0, max: 1 },
+        xaxis: { tickDecimals: 0 },
+        grid: { show: true, aboveData: true },
+        legend: {position: 'nw'}
+    });
+}
+
+//Copyright (c) 2009: Dan Dyar
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+
---
+++ trunk/site/FetalSmackdown2010/js/fetal_smackdown.js
@@ -0,0 +1,158 @@
+﻿function fetal_b(ega) {
+/*
+From:
+Predictors of Technical Success and Postnatal Biventricular Outcome
+After In Utero Aortic Valvuloplasty for Aortic Stenosis With 
+Evolving Hypoplastic Left Heart Syndrome.
+Circulation. 2009;120:1482-1490
+Published online before print September 28, 2009, doi: 10.1161/CIRCULATIONAHA.109.848994
+*/
+    this.ega = parseFloat(ega);
+    this.aov = {
+        mean: function() { return (0.02415 * ega) - 0.17158 },
+        sd: function() { return (0.00206 * ega) - 0.00519 },
+        zscore: function(score) { return (+score - this.mean()) / this.sd() },
+        limit: function(z) { return (z * this.sd()) + this.mean() }
+    };
+    this.lvMinor = {
+        mean: function() { return (0.05981 * ega) - 0.51997 },
+        sd: function() { return (0.00784 * ega) - 0.06281 },
+        zscore: function(score) { return (+score - this.mean()) / this.sd() },
+        limit: function(z) { return (z * this.sd()) + this.mean() }
+    };
+
+} //end fb constructor
+
+function fetal_rb(ega) {
+    /*
+    From:
+    Development of Z-scores for fetal cardiac dimensions from echocardiography.
+    Schneider C, McCrindle BW, Carvalho JS, Hornberger LK, McCarthy KP, Daubeney PE.
+    Ultrasound Obstet Gynecol. 2005 Nov;26(6):599-605.
+    */
+    this.ega = parseFloat(ega);
+    this.aov = {
+        "intercept": -5.019,
+        "multiplier": 1.263,
+        "rmse": 0.1282,
+        "zscore": function(score) {
+            var predicted = this.multiplier * Math.log(ega) + this.intercept;
+            return (Math.log(score) - predicted) / this.rmse;
+        },
+        "limit": function(zscore) {
+            var predicted = this.multiplier * Math.log(ega) + this.intercept;
+            return Math.exp(predicted + (zscore * this.rmse));
+        },
+        "mean": function() {
+            return Math.exp(this.multiplier * Math.log(ega) + this.intercept);
+        }
+    };
+    this.lvMinor = {
+        "intercept": -4.292,
+        "multiplier": 1.298,
+        "rmse": 0.1560,
+        "zscore": function(score) {
+            var predicted = this.multiplier * Math.log(ega) + this.intercept;
+            return (Math.log(score) - predicted) / this.rmse;
+        },
+        "limit": function(zscore) {
+            var predicted = this.multiplier * Math.log(ega) + this.intercept;
+            return Math.exp(predicted + (zscore * this.rmse));
+        },
+        "mean": function() {
+            return Math.exp(this.multiplier * Math.log(ega) + this.intercept);
+        }
+    };
+
+} // end frb constructor
+
+function fetal_mi(ega) {
+    /*
+    FROM:
+    Fetal echocardiography: z-score reference ranges for a large patient population
+    W. Lee et al
+    Ultrasound in Obstetrics and Gynecology
+    Volume 35 Issue 1, Pages 28 - 34
+    Published Online: 15 Dec 2009
+    */
+    this.ega = parseFloat(ega);
+    this.aov = {
+        "mean": function() { return ((0.288 * ega) - 2.59) / 10; },
+        "sd": function() { return ((0.0165 * ega) + 0.064) / 10; },
+        "zscore": function(score) { return (score - this.mean()) / this.sd(); },
+        "limit": function(z) { return (+z * this.sd()) + this.mean(); }
+    };
+    this.lvMinor = {
+        "mean": function() { return (0.513 * ega) - 3.81; },
+        "sd": function() { return (0.0394 * ega) + 0.059; },
+        "zscore": function(score) { return (+score - this.mean()) / this.sd(); },
+        "limit": function(z) { return (+z * this.sd()) + this.mean(); }
+    };
+} //end fetal_mi constructor
+
+$(function() {
+    // do something on document ready
+if ($.browser.msie) {
+    $('input:radio').click(function() {
+        this.blur();
+        this.focus();
+    });
+}
+
+$(':input').change(CalculateZScore);
+    
+});
+function CalculateZScore() {
+    var ega = parseInt($('#Weeksddl').val());
+    var score = parseFloat($('#txtaov').val());
+    var ref = $('input[name=output]:checked').val();
+    var fetal, range, data;
+    if(!isNaN(score)) {
+        switch (ref) {
+            case "Boston":
+                fetal = new fetal_b(ega);
+                break;
+            case "Royal":
+                fetal = new fetal_rb(ega);
+                break;
+            case "Michigan":
+                fetal = new fetal_mi(ega);
+                break;
+
+        }//end switch
+        $('#mean').text(fetal.aov.mean().toFixed(2));
+        var zscore = fetal.aov.zscore(score);
+        $('#zscore').text(zscore.toFixed(1));
+        var lower = fetal.aov.limit(-1.65).toFixed(2);
+        var upper = fetal.aov.limit(1.65).toFixed(2);
+        range = lower + " - " + upper;
+        $('#range').text(range);
+        $('.results').removeClass('normal borderline mild moderate severe').addClass(zscoreFlag(zscore));
+        data = [ega, score];
+        plotIt(data, ref);
+    }//end if
+
+
+} //end CalculateZScore fx
+
+
+
+//Copyright (c) 2009: Dan Dyar
+
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
