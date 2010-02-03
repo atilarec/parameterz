2b5b8b365bcbd915babc33e8eb4f45fb375c991f
dan.dyar <dan.dyar@5c7b53a3-c253-0410-9ef7-7744691651de>
2010-02-03 06:13:36 +0000

    avr data table

---
+++ site/ProstheticValves/js/AVR.js
@@ -0,0 +1,166 @@
+﻿var avr = [{
+    name: "ATS",
+    type: "Bileaflet",
+    sizes: [{
+        size: 19,
+        peak: [47, 12.6],
+        mean: [25.3, 8],
+        eoa: [1.1, 0.3]
+    },
+    {
+        size: 21,
+        peak: [23.7, 6.8],
+        mean: [15.9, 5],
+        eoa: [1.4, 0.5]
+    },
+    {
+        size: 23,
+        peak: [],
+        mean: [14.4, 4.9],
+        eoa: [1.7, 0.5]
+    },
+    {
+        size: 25,
+        peak: [],
+        mean: [11.3, 3.7],
+        eoa: [2.1, 0.7]
+    },
+    {
+        size: 27,
+        peak: [],
+        mean: [8.4, 3.7],
+        eoa: [2.5, 0.1]
+    },
+    {
+        size: 29,
+        peak: [],
+        mean: [8, 3],
+        eoa: [3.1, 0.8]
+}]
+    },
+{
+    name: "ATS AP",
+    type: "Bileaflet",
+    sizes: [{
+        size: 18,
+        peak: [],
+        mean: [21, 1.8],
+        eoa: [1.2, 0.3]
+    },
+    {
+        size: 20,
+        peak: [21.4, 4.2],
+        mean: [11.1, 3.5],
+        eoa: [1.3, 0.3]
+    },
+    {
+        size: 22,
+        peak: [18.7, 8.3],
+        mean: [10.5, 4.5],
+        eoa: [1.7, 0.4]
+    },
+    {
+        size: 24,
+        peak: [15.1, 5.6],
+        mean: [7.5, 3.1],
+        eoa: [2.0, 0.6]
+    },
+    {
+        size: 26,
+        peak: [],
+        mean: [6, 2],
+        eoa: [2.1, 0.4]
+}]
+    },
+{
+    name: "Baxter Perimount",
+    type: "Stented bovine pericardial",
+    sizes: [{
+        size: 19,
+        peak: [],
+        mean: [],
+        eoa: []
+    },
+    {
+        size: 21,
+        peak: [],
+        mean: [],
+        eoa: []
+    },
+    {
+        size: 23,
+        peak: [],
+        mean: [],
+        eoa: []
+    },
+    {
+        size: 25,
+        peak: [],
+        mean: [],
+        eoa: []
+    },
+    {
+        size: 27,
+        peak: [],
+        mean: [],
+        eoa: []
+}]
+    },
+{
+    name: "Biocor",
+    type: "Stented porcine",
+    sizes: [{
+        size: 23,
+        peak: [],
+        mean: [],
+        eoa: []
+    },
+    {
+        size: 25,
+        peak: [],
+        mean: [],
+        eoa: []
+    },
+    {
+        size: 27,
+        peak: [],
+        mean: [],
+        eoa: []
+}]
+    },
+{
+    name: "Extended Biocor",
+    type: "Stentless",
+    sizes: [{
+        size: '19-21',
+        peak: [],
+        mean: [],
+        eoa: []
+    },
+    {
+        size: 23,
+        peak: [],
+        mean: [],
+        eoa: []
+    },
+    {
+        size: 25,
+        peak: [],
+        mean: [],
+        eoa: []
+}]
+    }
+
+
+];
+
+/*
+example script:
+
+$.each(avr, function(key, value) { 
+alert(key + ': ' + value.sizes.length); 
+});
+
+*/
+
+    
