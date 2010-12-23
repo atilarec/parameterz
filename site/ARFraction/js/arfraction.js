236e4f704f88da169acc67042959bfbd31b55126
dan.dyar <dan.dyar@5c7b53a3-c253-0410-9ef7-7744691651de>
2010-12-23 05:28:24 +0000

    streamlined js; ran through jslint; minor mods to html (removed bsa options);

--- site/ARFraction/js/arfraction.js
+++ site/ARFraction/js/arfraction.js
@@ -1,35 +1,34 @@
-﻿function GetExponent(pvca_bsa, vti_ratio) {
-    if (pvca_bsa <= 0.05) {
-        return -2.62 + (31.42 * pvca_bsa) + (2.33 * vti_ratio);
-    }
-    else if (pvca_bsa > 0.05) {
-        return -1.18 + (2.66 * pvca_bsa) + (2.33 * vti_ratio);
-    }
-} //end GetExponent fx
-
-function CalculateRF() {
-    var bsa, ht, wt, method, pvc, pvca, pvca_bsa, vti_a, vti_r, vti_ratio, exponent, rf;
+﻿function CalculateRF() {
+    var bsa, ht, wt, pvc, pvca, pvca_bsa, vti_a, vti_r, vti_ratio, exponent, rf;
     ht = parseFloat($('#txtHT').val());
     wt = parseFloat($('#txtWT').val());
-    method = $('#cmbBSA').val();
     if (ht && wt) {
-        bsa = CalcBSA(ht, wt, method);
-    } else if (wt && method == 'Dreyer') {
-        bsa = CalcBSA('', wt, method);
+        bsa = CalcBSA(ht, wt, 'Haycock');
+    } else if (wt) {
+        bsa = CalcBSA('', wt, 'Dreyer');
     }
     else {
         return false;
     }
-    $('#BSA').text(bsa.toFixed(2));
+    $('#BSA').html(bsa.toFixed(2) + '&nbsp;m<sup>2</sup>').attr('title', 'BSA');
     pvc = parseFloat($('#txtPVC').val());
     vti_a = parseFloat($('#txtVTI-antegrade').val());
     vti_r = parseFloat($('#txtVTI-retro').val());
 
     if (bsa && pvc && vti_a && vti_r) {
+        //calculate the vena contracta area
         pvca = Math.PI * Math.pow((pvc / 2), 2);
+        //calculate the ratios
         pvca_bsa = pvca / bsa;
         vti_ratio = vti_r / vti_a;
-        exponent = GetExponent(pvca_bsa, vti_ratio);
+        //calculate the exponent
+        if (pvca_bsa <= 0.05) {
+            exponent = -2.62 + (31.42 * pvca_bsa) + (2.33 * vti_ratio);
+        }
+        else if (pvca_bsa > 0.05) {
+            exponent = -1.18 + (2.66 * pvca_bsa) + (2.33 * vti_ratio);
+        } 
+        //solve the rf
         rf = 0.6 / (1 + Math.exp(-exponent));
         $('#RF').text(rf.toFixed(2));
     }
@@ -38,5 +37,5 @@ function CalculateRF() {
 } // end calcRF fx
 
 function resetForm() {
-    $('.results').text('');
+    $('.results').text('').attr('title', '');
 }
