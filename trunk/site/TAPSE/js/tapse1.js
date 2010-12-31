e42a885eb362d5d07eb7d3a884d739358bf22cdb
dan.dyar <dan.dyar@5c7b53a3-c253-0410-9ef7-7744691651de>
2010-12-31 06:42:11 +0000

    added bits for charting with the google chart api

--- trunk/site/TAPSE/js/tapse1.js
+++ trunk/site/TAPSE/js/tapse1.js
@@ -43,10 +43,35 @@ var TAPSE = { //values are mean, lower limit (-3SD)
         .removeClass()
         .addClass(zscoreFlag(z))
         .attr('title', calcPercentile(z) + ' %-ile');
-        
+        chartIt(age, score)
     }//end if tapse
 
 }
 function resetForm() {
     $('#zscore,#mean,#range').text('').removeClass().attr('title', '');
+    $('#chart1').attr('src', 'http://chart.apis.google.com/chart?chxl=0:|0m|1m|3m|6m|1y|2y|3y|4y|5y|6y|7y|8y|9y|10y|11y|12y|13y|14y|15y|16y|17y|18y&chxr=0,0,5|1,0,4&chxt=x,y&chs=500x300&cht=lc&chco=224499,224499&chds=0,4,0,4&chd=t:1.15,1.42,1.65,1.77,1.88,1.94,2.02,2.07,2.13,2.18,2.25,2.28,2.3,2.31,2.36,2.43,2.54,2.65,2.75,2.78,2.88,2.91|0.68,0.85,1.01,1.13,1.25,1.36,1.48,1.56,1.6,1.62,1.64,1.67,1.73,1.79,1.83,1.84,1.85,1.87,1.93,1.98,2.04,2.05&chdl=%2B2+SD|-2+SD&chg=25,25&chls=0.75,-1,-1|0.75,-1,-1&chm=b,3399CC68,0,1,0&chtt=TAPSE+vs.+Age');
 }
+
+function chartIt(age, score) {
+    var ages = ['1m', '3m', '6m', '12m', '1y', '2y', '3y', '4y', '5y', '6y', '7y', '8y', '9y', '10y', '11y', '12y', '13y', '14y', '15y', '16y', '17y', '18y'];
+    var datapoint = $.inArray(age, ages);
+    var data_string = score;
+    for (var i = 1; i <= 21; i++) {
+        data_string += ',' + score;
+    }
+
+    var part1 = 'http://chart.apis.google.com/chart?chxl=0:|0|1m|3m|6m|1y|2y|3y|4y|5y|6y|7y|8y|9y|10y|11y|12y|13y|14y|15y|16y|17y|18y&chxr=0,0,5|1,0,4&chxt=x,y&chs=500x300&cht=lc&chco=224499,224499,000000&chds=0,4,0,4,0,4&chd=t2:1.15,1.42,1.65,1.77,1.88,1.94,2.02,2.07,2.13,2.18,2.25,2.28,2.3,2.31,2.36,2.43,2.54,2.65,2.75,2.78,2.88,2.91|0.68,0.85,1.01,1.13,1.25,1.36,1.48,1.56,1.6,1.62,1.64,1.67,1.73,1.79,1.83,1.84,1.85,1.87,1.93,1.98,2.04,2.05|';
+    var part2 = '&chdl=%2B2+SD|-2+SD|your+data&chg=25,25&chls=0.75,-1,-1|0.75,-1,-1|1&chm=d,000000,2,';
+    var part3 = ',10|b,3399CC68,0,1,0&chtt=TAPSE+vs.+Age';
+
+    var url = part1;
+    url += data_string;
+    url += part2;
+    url += datapoint;
+    url += part3;
+
+    $('#chart1').attr('src', url);
+    var link_url = url.replace('&chs=500x300', '&chs=600x400')
+    $('#chart_link').attr('href', link_url);
+    
+}//end chartIt fx
